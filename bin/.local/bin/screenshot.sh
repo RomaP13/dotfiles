05679c54391bf60e1d06422c5f74f356e6fbba08
Roman Pomazan <romapomazan1310@gmail.com>
2026-07-19 23:50:44 +0300

    refactor(bin): rename scripts and remove redundant ones

--- bin/.local/bin/screenshot.sh
+++
@@ -1,1 +0,0 @@
-xfce4-screenshooter -f -s "$HOME/Pictures/Screenshots/screenshot_$(date +%Y-%m-%d_%H-%M-%S).png"
--- bin/.local/bin/set_random_wallpaper_xfce4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-WALLPAPER_DIR="/home/roman/Pictures/wallpapers"
-
-DIR=$(find $WALLPAPER_DIR/* -type d | shuf -n 1)
-
-WALLPAPER=$(ls "$DIR" | shuf -n 1)
-
-xfce4-set-wallpaper "$DIR/$WALLPAPER"
